ea641c8f87c8e1a66a664850aa1756ee9fc327f4
Ethan Mick <ethanmski@gmail.com>
2013-08-08 23:20:57 -0400

    Added validation to the Feed object.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -6,6 +6,8 @@ class Feed < ActiveRecord::Base
   has_many :links
   has_many :entries
 
+  validates :title, :source_url, presence: true
+
 
   def update_feed
     self.send "update_entries_#{self.source_type}", self
